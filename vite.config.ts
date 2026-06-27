1b0ea187f623fade3bee1f2436ad8d88185fcf46
Shreyas <oyedrizzle@gmail.com>
2026-06-27 14:57:37 +0530

    fix: remove deprecated esbuild option for vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    target: 'esnext'
-  },
   build: {
     cssCodeSplit: true,
-    minify: 'esbuild',
     target: 'esnext',
     rollupOptions: {
       output: {
