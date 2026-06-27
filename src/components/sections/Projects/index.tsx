6402c6935613d2a2363c6e2f3a1f3c107c41498f
Shreyas <oyedrizzle@gmail.com>
2026-06-27 17:51:05 +0530

    fix: thread width, saturn bounce sync, and dynamic scrollable experience panels

--- src/components/sections/Projects/index.tsx
+++ src/components/sections/Projects/index.tsx
@@ -25,41 +25,44 @@ const Projects = () => {
           <PerspectiveCamera makeDefault position={[0, 0, 8]} fov={45} />
           <group position={[0, 1.5, 0]}>
             <Saturn />
+            <Html center position={[0, -0.15, 0]} zIndexRange={[10, 20]}>
+              <div style={{ width: '100vw', height: '100vh', pointerEvents: 'none', display: 'flex', justifyContent: 'center' }}>
+                {/* 2D Hanging Threads Overlay */}
+                <div className="projects-overlay-container projects-threads-overlay">
+                  <div className="hanging-thread" style={{ left: '0%',  height: '70%' }} />
+                  <div className="hanging-thread" style={{ left: '17%', height: '85%' }} />
+                  <div className="hanging-thread" style={{ left: '33%', height: '65%' }} />
+                  <div className="hanging-thread" style={{ left: '50%', height: '95%' }} />
+                  <div className="hanging-thread" style={{ left: '67%', height: '80%' }} />
+                  <div className="hanging-thread" style={{ left: '83%', height: '75%' }} />
+                  <div className="hanging-thread" style={{ left: '100%', height: '85%' }} />
+
+                  <div className="projects-swarm-mask">
+                    {particles.map((p, i) => (
+                      <div
+                        key={i}
+                        className="thread-particle"
+                        style={{
+                          left: p.left,
+                          top: p.top,
+                          animation: `${p.direction === 1 ? 'swarmDown' : 'swarmUp'} ${p.speed} linear ${p.animDelay} infinite`
+                        }}
+                      />
+                    ))}
+                  </div>
+                </div>
+
+                {/* 2D Line-Art Planets */}
+                <div className="projects-overlay-container projects-nodes-overlay" style={{ pointerEvents: 'auto' }}>
+                  <ProjectNodes
+                    setHoveredPlanet={setHoveredPlanet}
+                    onTapPlanet={handlePlanetTap}
+                  />
+                </div>
+              </div>
+            </Html>
           </group>
         </View>
-      </div>
-
-      {/* 2D Hanging Threads Overlay */}
-      <div className="projects-overlay-container projects-threads-overlay">
-        <div className="hanging-thread" style={{ left: '0%',  height: '70%' }} />
-        <div className="hanging-thread" style={{ left: '17%', height: '85%' }} />
-        <div className="hanging-thread" style={{ left: '33%', height: '65%' }} />
-        <div className="hanging-thread" style={{ left: '50%', height: '95%' }} />
-        <div className="hanging-thread" style={{ left: '67%', height: '80%' }} />
-        <div className="hanging-thread" style={{ left: '83%', height: '75%' }} />
-        <div className="hanging-thread" style={{ left: '100%', height: '85%' }} />
-
-        <div className="projects-swarm-mask">
-          {particles.map((p, i) => (
-            <div
-              key={i}
-              className="thread-particle"
-              style={{
-                left: p.left,
-                top: p.top,
-                animation: `${p.direction === 1 ? 'swarmDown' : 'swarmUp'} ${p.speed} linear ${p.animDelay} infinite`
-              }}
-            />
-          ))}
-        </div>
-      </div>
-
-      {/* 2D Line-Art Planets */}
-      <div className="projects-overlay-container projects-nodes-overlay">
-        <ProjectNodes
-          setHoveredPlanet={setHoveredPlanet}
-          onTapPlanet={handlePlanetTap}
-        />
       </div>
 
       {/* Planet label HUD
