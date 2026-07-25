214d493952370802670176bf981ac13ad2ece0f2
Shreyas <oyedrizzle@gmail.com>
2026-07-25 15:59:35 +0530

    fix: perfectly align project planets with hanging svg threads across all viewports

--- src/components/sections/Projects/ProjectNodes.tsx
+++ src/components/sections/Projects/ProjectNodes.tsx
@@ -192,25 +192,25 @@ const ProjectNodes = ({ isTouchPrimary, setHoveredPlanet, onTapPlanet }: Project
 
   return (
     <>
-      <PlanetHitArea name="Lucy" visualSize={sm} top="22%" left="0%"
+      <PlanetHitArea name="Lucy" visualSize={sm} top="22%" left="27.27%"
         isTouchPrimary={isTouchPrimary} setHoveredPlanet={setHoveredPlanet}
         onTapPlanet={onTapPlanet} hoverAnim={HOVER_CW}>
         <PlanetGasGiant />
       </PlanetHitArea>
 
-      <PlanetHitArea name="Alethia" visualSize={sm} top="38%" left="66.67%"
+      <PlanetHitArea name="Alethia" visualSize={sm} top="38%" left="57.58%"
         isTouchPrimary={isTouchPrimary} setHoveredPlanet={setHoveredPlanet}
         onTapPlanet={onTapPlanet} hoverAnim={HOVER_CW}>
         <PlanetCratered />
       </PlanetHitArea>
 
-      <PlanetHitArea name="Valerie" visualSize={lg} top="54%" left="33.33%"
+      <PlanetHitArea name="Valerie" visualSize={lg} top="54%" left="42.42%"
         isTouchPrimary={isTouchPrimary} setHoveredPlanet={setHoveredPlanet}
         onTapPlanet={onTapPlanet} hoverAnim={HOVER_CCW}>
         <PlanetRinged />
       </PlanetHitArea>
 
-      <PlanetHitArea name="Relay" visualSize={sm} top="70%" left="100%"
+      <PlanetHitArea name="Relay" visualSize={sm} top="70%" left="72.73%"
         isTouchPrimary={isTouchPrimary} setHoveredPlanet={setHoveredPlanet}
         onTapPlanet={onTapPlanet} hoverAnim={HOVER_CCW}>
         <PlanetEarthLike />
